761ddc0358f1592c8cc3eede97c3100fad971e0a
IlIXXIlI <127801939+IlIXXIlI@users.noreply.github.com>
2026-01-28 12:01:30 +0300

    Added Diff tables

--- main.cpp
+++ main.cpp
@@ -30,10 +30,38 @@ std::function<double(double)> createLagrangeInterpolator(const std::vector<std::
     };
 }
 
+double q(const std::vector<std::pair<double, double>>& data) {
+    size_t n = data.size();
+    return (data[n - 1].first - data[0].first)/ (data[1].first - data[0].first );
+}
+
+std::vector<std::vector<double>> getDiffTable(const std::vector<std::pair<double, double>>& data) {
+    std::vector<std::vector<double>> result;
+    result.reserve(data.size());
+    for (auto i : data) {
+        result[0].push_back(i.second);
+    }
+
+    for (size_t i = 1; i < data.size(); i++) {
+        result[i].push_back({});
+        for (size_t j = 0, szi = result[i-1].size(); j < szi; j++) {
+            result[i].push_back(result[i-1][j] - result[i-1][j-1]);
+        }
+    }
+
+    return result;
+}
+
 int main() {
     auto func = createLagrangeInterpolator(parseCvv("/Users/dima.../CLionProjects/LAB_3/dataset_02.csv"));
     std::cout<< "Point 0.1: " << func(0.1) << std::endl;
     std::cout<< "Point 0.6: " << func(0.6) << std::endl;
     std::cout<< "Point 1.2: " << func(1.2) << std::endl;
+    // std::cout<< "Point 1.2: " <<  << std::endl;
+    for (auto s : getDiffTable(parseCvv("/Users/dima.../CLionProjects/LAB_3/dataset_02.csv"))) {
+        for (auto g : s) {
+            std::cout<< "table: " << g << std::endl;
+        }
+    }
     return 0;
 }
